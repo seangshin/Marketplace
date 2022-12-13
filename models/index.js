39603939dbb094d558e1026e2ef95e2db9aef2ec
Sean Shin <shin.seang@gmail.com>
2022-12-13 02:39:38 -0500

    added comment model for bidder functionality

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 //import models
 const User = require('./User');
 const Bid = require('./Bid');
+const Comment = require('./Comment');
 
 //association methods for the Sequelize models to create relationships between them
 //A user can have many Bids
@@ -14,4 +15,26 @@ User.hasMany(Bid, {
     foreignKey: 'user_id',
   });
 
-  module.exports = { User, Bid };
+  //A user can have many comments
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+//A comment belongs to a single user
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+//A post can have many comments
+Bid.hasMany(Comment, {
+  foreignKey: 'bid_id',
+  onDelete: 'CASCADE'
+});
+
+//A comment belongs to a single post
+Comment.belongsTo(Bid, {
+  foreignKey: 'bid_id',
+});
+
+  module.exports = { User, Bid , Comment};
