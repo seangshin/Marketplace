60544febf7b6e7adc5f8ed399de061a4a2e14b71
Sean Shin <shin.seang@gmail.com>
2022-12-13 13:48:59 -0500

    added bidding functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const bidRoutes = require('./bidRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
 router.use('/bids', bidRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
---
+++ public/js/bid.js
@@ -0,0 +1,75 @@
+const bidFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the bid form
+    const price = document.querySelector('#userBid').value.trim();
+    const bidId = event.target.getAttribute('bid-id');
+    const startingBid = document.querySelector('#startingBid').textContent;
+    const startingBidNum = parseInt(startingBid);
+    alert(`Bid ID: ${bidId} and bid amount ${price}`);
+    console.log(startingBidNum);
+    console.log(typeof startingBidNum);
+
+    //check if user's bid is higher than starting bid
+    if (price<startingBidNum) {
+        alert('Bids must be higher than the starting Bid. Please try again.');
+        document.location.replace(`/bid/${bidId}`);
+    } else {
+        if (price) {
+            //GET request to retrieve all comments
+            const result = await fetch('/api/comments', {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+              });
+            const comments = await result.json();
+              //console.log(comments);
+
+            //filter out comments applicable to the current bid
+            const thisComments = comments.filter(comment => {
+                return comment.bid_id == bidId;
+            });
+
+            //check if current bid is higher than other bids
+            let check = false;
+            thisComments.forEach(comment => {
+                if(comment.price>price) {
+                    check=true;
+                }
+            });
+            if (check) {
+                alert("Bids must be higher than all other bids. Please try again.");
+                document.location.replace(`/bid/${bidId}`);
+            } else {
+                const bid_id = bidId;
+                // Send a POST request to the API endpoint
+                const response = await fetch('/api/comments', {
+                    method: 'POST',
+                    body: JSON.stringify({ price, bid_id }),
+                    headers: { 'Content-Type': 'application/json' },
+                });
+
+                if (response.ok) {
+                 // If successful, redirect the browser
+                    document.location.replace(`/bid/${bidId}`);
+                    alert(`Bid added.`);
+                } else {
+                    alert(`Bid not added.`);
+                }
+            }
+          
+      
+          
+        }
+    }
+  
+    
+};
+  
+  
+  if (document.querySelector('.submitBtn') != null) {
+    document
+    .querySelector('.submitBtn')
+    .addEventListener('click', bidFormHandler);
+  }
+  
+  
